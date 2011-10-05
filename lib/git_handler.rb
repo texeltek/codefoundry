edc5cc43de4bc9d5aa3086014606e5c6941e2d8f
Andrew Curry <acurry@texeltek.com>
2011-10-05 11:25:34 -0400

    Added debug logs to git_handler

--- lib/git_handler.rb
+++ lib/git_handler.rb
@@ -20,9 +20,19 @@ class GitHandler
   def call( env )
     # if the request is for a git repository (name.git), then send the request to
     # the git handler
+    # Rails.logger.debug "before match, env['PATH_INFO'] = #{env['PATH_INFO']}"
     if m = MATCH_RE.match( env['PATH_INFO'] )
+      
+      # spit out GIT_SSL_CERT
+      # Rails.logger.debug "we have a git request"
+      # Rails.logger.debug "ssl server DN = #{ENV['SSL_SERVER_S_DN']}"
+      # Rails.logger.debug "ssl client DN = #{ENV['SSL_CLIENT_S_DN']}" 
+      # Rails.logger.debug "script url    = #{ENV['SCRIPT_URL']}"
+      
       # remove the prefix
       env['PATH_INFO'] = "/git/#{m[1]}/#{m[2]}.git#{m[3]}"
+      
+      # Rails.logger.debug "git env['PATH_INFO'] = #{env['PATH_INFO']}"
       @git_provider.call( env )
     else
       @app.call( env )
