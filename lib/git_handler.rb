71a934e72733987a9594986e3d4200f91e7477a9
Andrew Curry <acurry@texeltek.com>
2011-10-07 15:04:55 -0400

    git access control & project-scope authentication
    CF now uses project-scope authentication to
    fulfill or deny git requests over HTTPS (required).
    
    If the user does not have a DN
    as an attribute of their CF account,
    CF returns HTTP 401 (Unauthorized).
    
    if the project or repo does not exist
    based on its name parsed from the request,
    CF returns HTTP 404 (Not Found).
    
    If the project does not have a privilege
    for the user, CF returns HTTP 403 (Forbidden).

--- lib/git_handler.rb
+++ lib/git_handler.rb
@@ -1,6 +1,8 @@
 class GitHandler
   MATCH_RE = Regexp.new('^/projects/(\\w+)/repositories/(\\w+)\.git(/.*)')
-  GIT_SHORTCUT_RE = Regexp.new('/git/(\\w+)/(\\w+)\.git(/.*)')
+  GIT_SHORTCUT_RE = Regexp.new('^/git/(\\w+)/(\\w+)\.git(/.*)')
+
+  PLAIN_TYPE = {"Content-Type" => "text/plain"}
 
   def initialize( app )
     @logger = ActiveRecord::Base.logger
@@ -19,12 +21,43 @@ class GitHandler
   end
 
   def call( env )
+
     # if the request is for a git repository (name.git), then send the request to
     # the git handler
-    if m = MATCH_RE.match( env['PATH_INFO'] ) || m = GIT_SHORTCUT_RE.match( env['PATH_INFO'] ) 
-      # remove the prefix
-      env['PATH_INFO'] = "/git/#{m[1]}/#{m[2]}.git#{m[3]}"
-      @git_provider.call( env )
+    short_match = GIT_SHORTCUT_RE.match( env['PATH_INFO'] )
+    long_match = MATCH_RE.match( env['PATH_INFO'] )
+
+    m = short_match if short_match
+    m = long_match if long_match
+
+    if m
+      # look for the user, project and repository in CF
+      user = User.find_by_x509_dn    env['SSL_CLIENT_S_DN']
+      proj = Project.find_by_name    m[1]
+      repo = Repository.find_by_name m[2]
+
+
+      if !user
+        Rails.logger.error "User #{m[1]} not found in CodeFoundry"
+        return [401, PLAIN_TYPE, [""]]
+
+      elsif !proj
+        Rails.logger.error "Project #{m[1]} not found in CodeFoundry"
+        return [404, PLAIN_TYPE, [""]]
+
+      elsif !repo
+        Rails.logger.error "Repository #{m[2]} not found in project #{m[1]}"
+        return [404, PLAIN_TYPE, [""]]
+
+      elsif !proj.users.include? user
+        Rails.logger.error "User #{user.username} does not have access"
+        return [403, PLAIN_TYPE, [""]]
+
+      else
+        # remove the prefix
+        env['PATH_INFO'] = "/git/#{m[1]}/#{m[2]}.git#{m[3]}"
+        @git_provider.call( env )
+      end
     else
       @app.call( env )
     end
