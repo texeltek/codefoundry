6925b33ce1377a07cad6dabe14e1ee6d73e0bae3
Andrew Curry <acurry@texeltek.com>
2011-10-10 13:18:49 -0400

    git read&write access control
    Enabled git read&write access control
    
    Administrators & Committers can read from and write to repositories:
      - git push
      - git clone, git fetch, git pull
    Reviewers & Testers can only read from repositories:
      - git clone, git fetch, git pull
    
    GitHandlers looks at env['QUERY_STRING'] and looks
    for either 'git-upload-pack' or 'git-receive-pack'

--- lib/git_handler.rb
+++ lib/git_handler.rb
@@ -1,6 +1,8 @@
 class GitHandler
-  MATCH_RE = Regexp.new('^/projects/(\\w+)/repositories/(\\w+)\.git(/.*)')
+  MATCH_RE        = Regexp.new('^/projects/(\\w+)/repositories/(\\w+)\.git(/.*)')
   GIT_SHORTCUT_RE = Regexp.new('^/git/(\\w+)/(\\w+)\.git(/.*)')
+  GIT_READ_RE     = Regexp.new('git-upload-pack')  # git clone, git fetch, git pull
+  GIT_WRITE_RE    = Regexp.new('git-receive-pack') # git push
 
   PLAIN_TYPE = {"Content-Type" => "text/plain"}
 
@@ -24,19 +26,22 @@ class GitHandler
 
     # if the request is for a git repository (name.git), then send the request to
     # the git handler
-    short_match = GIT_SHORTCUT_RE.match( env['PATH_INFO'] )
-    long_match = MATCH_RE.match( env['PATH_INFO'] )
+    short_match = GIT_SHORTCUT_RE.match env['PATH_INFO'] 
+    long_match = MATCH_RE.match env['PATH_INFO'] 
 
     m = short_match if short_match
     m = long_match if long_match
 
     if m
+
       # look for the user, project and repository in CF
       user = User.find_by_x509_dn    env['SSL_CLIENT_S_DN']
       proj = Project.find_by_name    m[1]
       repo = Repository.find_by_name m[2]
-
-
+      
+      r_action = GIT_READ_RE.match  env['QUERY_STRING'] 
+      w_action = GIT_WRITE_RE.match env['QUERY_STRING']
+      
       if !user
         Rails.logger.error "User #{m[1]} not found in CodeFoundry"
         return [401, PLAIN_TYPE, [""]]
@@ -50,7 +55,11 @@ class GitHandler
         return [404, PLAIN_TYPE, [""]]
 
       elsif !proj.users.include? user
-        Rails.logger.error "User #{user.username} does not have access"
+        Rails.logger.error "User #{user.username} does not have access to project #{m[1]}"
+        return [403, PLAIN_TYPE, [""]]
+
+      elsif w_action && !proj.committer?(user)
+        Rails.logger.error "User #{user.username} does not have write access to project #{m[1]}"
         return [403, PLAIN_TYPE, [""]]
 
       else
