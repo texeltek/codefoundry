a1d3a3817763e4d1b8c53289653c215b62159fe1
Andrew Curry <acurry@texeltek.com>
2011-10-18 17:04:21 -0400

    filter out :password_confirmation from logfiles
    added :password_confirmation to CodeFoundry's application
    config so the field's value will not be logged.

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,6 @@ module Codefoundry
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
   end
 end
